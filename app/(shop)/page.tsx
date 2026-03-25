b77ca55d76cdc128432347a0c842b7ed42a2b923
Abdulmujeeb Awodi <awodiabdulmujeeb@gmail.com>
2026-03-25 11:14:00 +0100

    feat: Add initial shop page with global styling, product data, and a hero slider.

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { show, trending } from "@/lib/constants";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Link from "next/link";
-
 
 import {
   ArrowRight,
@@ -16,98 +15,18 @@ import {
   Watch,
 } from "lucide-react";
 
-interface ArrowProps {
-  className?: string;
-  style?: React.CSSProperties;
-  onClick?: () => void;
-}
-
-function SampleNextArrow(props: ArrowProps) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{
-        ...style,
-        display: style?.display || "block",
-        // background: "var(--muted)",
-        borderRadius: "50%",
-        right: "10px",
-        zIndex: 10,
-      }}
-      onClick={onClick}
-    />
-  );
-}
-
-function SamplePrevArrow(props: ArrowProps) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{
-        ...style,
-        display: style?.display || "block",
-        // background: "var(--muted)",
-        borderRadius: "50%",
-        left: "10px",
-        zIndex: 10,
-      }}
-      onClick={onClick}
-    />
-  );
-}
+const settings = {
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 6000,
+  pauseOnHover: true,
+  arrows: false,
+};
 
 const Page = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 6000,
-    pauseOnHover: true,
-    arrows: false,
-  };
-
-  const setting = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-        },
-      },
-    ],
-  };
-
   return (
     <div className="p-6">
       <Slider {...settings}>
@@ -145,12 +64,12 @@ const Page = () => {
                   </button>
                 </div>
               </div>
-              <div className="right-side relative  w-100 h-89 sm:h-[100vh] lg:w-150 lg:h-150">
+              <div className="right-side relative  w-full h-89 sm:h-[100vh] lg:w-150 lg:h-150">
                 <Image
                   src={item.image}
                   alt={item.name}
                   fill
-                  className="object-cover rounded-lg lg:rounded-none lg:scale-100"
+                  className="w-full h-auto object-cover rounded-lg lg:rounded-none lg:scale-100"
                 />
               </div>
               <div className=" flex lg:hidden gap-4 py-5 w-full justify-around">
@@ -165,45 +84,47 @@ const Page = () => {
           </div>
         ))}
       </Slider>
-      <div className="flex container w-full justify-center border-b border-[var(--border)] my-8  relative">
+
+      <div className="flex container w-full justify-center border-b border-[var(--border)] my-8 relative">
         <div className="w-90 flex  justify-between ">
-          <Link href="/products?category=laptop" className="group relative hover:border-b-2  border-[var(--accent)] flex flex-col gap-2 py-2 cursor-pointer items-center">
-            <div
-              className="group-hover:text-[var(--accent)] text-[var(--muted)] transition-colors"
-            >
+          <Link
+            href="/products?category=laptop"
+            className="group relative hover:border-b-2  border-[var(--accent)] flex flex-col gap-2 py-2 cursor-pointer items-center"
+          >
+            <div className="group-hover:text-[var(--accent)] text-[var(--muted)] transition-colors">
               <Laptop size={20} />
             </div>
             <p className="group-hover:text-[var(--foreground)] text-[var(--muted)] transition-colors text-sm">
               Laptop
             </p>
-
-
-
           </Link>
-          <Link href="/products?category=Smartphone" className="group relative hover:border-b-2  border-[var(--accent)] flex flex-col gap-2 py-2 cursor-pointer items-center">
-            <div
-              className="group-hover:text-[var(--accent)] text-[var(--muted)] transition-colors"
-            >
+          <Link
+            href="/products?category=Smartphone"
+            className="group relative hover:border-b-2  border-[var(--accent)] flex flex-col gap-2 py-2 cursor-pointer items-center"
+          >
+            <div className="group-hover:text-[var(--accent)] text-[var(--muted)] transition-colors">
               <Smartphone size={20} />
             </div>
             <p className="group-hover:text-[var(--foreground)] text-[var(--muted)] transition-colors text-sm">
               Smartphone
             </p>
           </Link>
-          <Link href="/products?category=Headphones" className="group relative hover:border-b-2  border-[var(--accent)] flex flex-col gap-2 py-2 cursor-pointer items-center">
-            <div
-              className="group-hover:text-[var(--accent)] text-[var(--muted)] transition-colors"
-            >
+          <Link
+            href="/products?category=Headphones"
+            className="group relative hover:border-b-2  border-[var(--accent)] flex flex-col gap-2 py-2 cursor-pointer items-center"
+          >
+            <div className="group-hover:text-[var(--accent)] text-[var(--muted)] transition-colors">
               <Headphones size={20} />
             </div>
             <p className="group-hover:text-[var(--foreground)] text-[var(--muted)] transition-colors text-sm">
               Headphones
             </p>
           </Link>
-          <Link href="/products?category=Watch" className="group relative hover:border-b-2  border-[var(--accent)] flex flex-col gap-2 py-2 cursor-pointer items-center">
-            <div
-              className="group-hover:text-[var(--accent)] text-[var(--muted)] transition-colors"
-            >
+          <Link
+            href="/products?category=Watch"
+            className="group relative hover:border-b-2  border-[var(--accent)] flex flex-col gap-2 py-2 cursor-pointer items-center"
+          >
+            <div className="group-hover:text-[var(--accent)] text-[var(--muted)] transition-colors">
               <Watch size={20} />
             </div>
             <p className="group-hover:text-[var(--foreground)] text-[var(--muted)] transition-colors text-sm">
@@ -212,30 +133,33 @@ const Page = () => {
           </Link>
         </div>
       </div>
-      <div className="w-full overflow-hidden">
-        <h2 className="text-lg sm:text-2xl font-bold text-[var(--foreground)] mb-1">
-          Trending Products
-        </h2>
-        <div className="flex justify-between items-center mb-8">
-          <p className="text-xs sm:text-sm text-[var(--muted)]">
-            Our most popular gear this week.
-          </p>
-          <Link
-            href="/"
-            className="text-xs sm:text-sm text-[var(--accent)] relative flex flex-row gap-1"
-          >
-            View All <ArrowRight className="w-4 h-4 sm:w-5 sm:h-5" />
-          </Link>
+      <div className="w-full overflow-hidden border-y border-[var(--muted)] py-4  rounded-lg">
+        <div className="outline-none">
+          <h2 className="text-lg sm:text-2xl font-bold text-[var(--foreground)] mb-1">
+            Trending Products
+          </h2>
+          <div className="flex justify-between items-center mb-8">
+            <p className="text-xs sm:text-sm text-[var(--muted)]">
+              Our most popular gear this week.
+            </p>
+            <Link
+              href="/products"
+              className="text-xs sm:text-sm text-[var(--accent)] relative flex flex-row gap-1"
+            >
+              View All <ArrowRight className="w-4 h-4 sm:w-5 sm:h-5" />
+            </Link>
+          </div>
         </div>
-        <Slider {...setting}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
           {trending.map((item) => (
-            <div key={item.id} className="outline-none">
-              <div className="flex flex-col  justify-center gap-2 relative max-w-sm rounded-sm overflow-hidden cursor-pointer">
+            <div key={item.id} className=" outline-none">
+              <div className="flex flex-col   gap-4 relative max-w-sm rounded-sm overflow-hidden cursor-pointer">
                 <Image
                   src={item.image}
                   alt={item.name}
                   width={248}
                   height={248}
+                  className="w-full h-auto object-cover"
                 />
                 <div className="flex flex-col justify-center gap-1 p-2">
                   <p className="text-[var(--muted)] text-sm">{item.type}</p>
@@ -247,9 +171,9 @@ const Page = () => {
               </div>
             </div>
           ))}
-        </Slider>
+        </div>
       </div>
-      <div className="w-full mt-12 bg-[var(--foreground)] rounded-lg flex flex-col items-center justify-center gap-2 py-20 px-4 ">
+      <div className="w-full mt-12 bg-[var(--foreground)] rounded-lg relative flex flex-col items-center justify-center gap-2 py-20 px-4 ">
         <h3 className="text-2xl font-bold text-[var(--background)]">
           Stay ahead of the curve
         </h3>
@@ -257,8 +181,7 @@ const Page = () => {
           Join our inner circle for early access to product launches and
           exclusive studio drops.
         </p>
-        <form className="flex justify-center items-center py-4 gap-4 flex-col sm:flex-row">
-
+        <form className="flex relative justify-center items-center py-4 gap-4 flex-col sm:flex-row">
           <div className="sm:w-sm w-xs">
             <input
               className="bg-[var(--background)] appearance-none border-2 border-[var(--muted)] rounded w-full py-2 px-4 text-[var(--muted)] leading-tight focus:outline-none focus:bg-white focus:border-[var(--accent)]"
